568bad69bdf5e4a10294ce87ea50f932f1307238
JK7k <97955908+JK7k@users.noreply.github.com>
2022-12-21 11:15:18 +0100

    Add files via upload

---
+++ sprawdzian3/walidacja.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Dane rejestracyjne</title>
+</head>
+<body>
+  <header>
+    <h1>Rejestracja pojazdów</h1>
+  </header>
+  <main>
+    <form>
+      <label for="brand">Marka pojazdu:</label><br>
+      <input type="text" id="brand" name="brand"><br>
+      <label for="model">Model pojazdu:</label><br>
+      <input type="text" id="model" name="model"><br>
+      <label for="registration_number">Numer rejestracyjny:</label><br>
+      <input type="text" id="registration_number" name="registration_number"><br>
+      <label for="fuel_type">Rodzaj paliwa:</label><br>
+      <select id="fuel_type" name="fuel_type">
+        <option value="gasoline">Benzyna</option>
+        <option value="diesel">Olej napędowy</option>
+        <option value="gas">Gaz</option>
+      </select><br>
+      <label for="customer_number">Numer klienta:</label><br>
+      <input type="number" id="customer_number" name="customer_number"><br><br>
+      <input type="submit" value="Zatwierdź dane">
+    </form> 
+  </main>
+  <footer>
+    <p>Imię i nazwisko</p>
+  </footer>
+  <script>
+    // Walidacja formularza po stronie przeglądarki
+    const form = document.querySelector('form');
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+
+      const brand = document.querySelector('#brand').value;
+      const model = document.querySelector('#model').value;
+      const registrationNumber = document.querySelector('#registration_number').value;
+      const customerNumber = document.querySelector('#customer_number').value;
+
+      if (!brand || !model || !registrationNumber || !customerNumber) {
+        alert('Proszę uzupełnić wszystkie pola');
+        return;
+      }
+
+      if (registrationNumber.length < 6) {
+        alert('Numer rejestracyjny musi składać się z co najmniej 6 znaków');
+        return;
+      }
+
+      if (customerNumber % 2 !== 0) {
+        alert('Numer klienta musi być parzysty');
+        return;
+      }
+
+      // W przypadku poprawnej walidacji wyświetl dane na stronie
+      
